668a9a73ce50c44e225d639d821bede5540441f3
Gulchak Zakhar <gulchak.zahar@gmail.com>
2017-04-20 17:33:32 +0300

    Added redux to courses component.

--- src/components/course/coursesComponent.js
+++ src/components/course/coursesComponent.js
@@ -21,13 +21,18 @@ class CoursesComponent extends React.Component {
     }
 
     onClickSave() {
-        this.props.dispatch(courseActions.createCourse(this.state.course));
+        this.props.createCourse(this.state.course);
+    }
+
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>;
     }
 
     render() {
         return (
             <div>
                 <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
                 <h2>Add Course</h2>
 
                 <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
@@ -38,10 +43,21 @@ class CoursesComponent extends React.Component {
     }
 }
 
+CoursesComponent.propTypes = {
+    courses: PropTypes.array.isRequired,
+    createCourse: PropTypes.func.isRequired
+};
+
 function mapStateToProps(state, ownProps) {
     return {
         courses: state.courses
     };
 }
 
-export default connect(mapStateToProps)(CoursesComponent);
+function mapDispatchToProps(dispatch) {
+    return {
+        createCourse: (course) => dispatch(courseActions.createCourse(course))
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesComponent);
