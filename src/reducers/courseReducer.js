5d6a65d10f13c7f41636909324a371e6768d8d8f
Gulchak Zakhar <gulchak.zahar@gmail.com>
2017-04-20 17:51:26 +0300

    Fixed bug in course reducer.

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,7 +1,6 @@
 export default function courseReducer(state = [], action) {
     switch (action.type) {
         case 'CREATE_COURSE':
-            state.push(action.course);
             return [...state,
                 Object.assign({}, action.course)
             ];
